82c5339089af5ae0e9ae1e2fefe6bef58fea5db7
Yildirim Bayazit AKYUREK <ybakyurek@gmail.com>
2023-03-29 16:14:00 +0300

    Odev2

---
+++ postgreSQL/Odev2.sql
@@ -0,0 +1,23 @@
+/* Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1. film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 
+16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+
+2.actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 
+'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+
+3.film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE 
+replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+Kolay Gelsin.
+*/
+
+--1
+select * from film where replacement_cost between 12.99 and 16.99;
+
+--2
+select first_name, last_name from actor where first_name IN ('Penelope','Nick' , 'Ed');
+
+--3
+select * from film where rental_rate IN (0.99, 2.99, 4.99) and replacement_cost IN (12.99, 15.99, 28.99);
