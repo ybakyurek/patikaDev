2f00fec908a373597e46ed8e50be42de138ede2c
Yildirim Bayazit AKYUREK <ybakyurek@gmail.com>
2023-03-30 10:00:29 +0300

    Odev6

---
+++ postgreSQL/Odev6.sql
@@ -0,0 +1,18 @@
+/*Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+2.film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+3.film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+4.film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+Kolay Gelsin.*/
+
+--1
+select avg(rental_rate) from film;
+--2
+select count(*) from film where title like 'C%';
+--3
+select length from film where rental_rate=0.99 order by length desc limit 1;
+--4
+select count(Distinct replacement_cost) from film where length>150;
