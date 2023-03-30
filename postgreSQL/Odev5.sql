914392807df87ebfbb6bbb4a2a81643218e963c0
Yildirim Bayazit AKYUREK <ybakyurek@gmail.com>
2023-03-30 09:55:35 +0300

    Odev5

---
+++ postgreSQL/Odev5.sql
@@ -0,0 +1,15 @@
+/*Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1. film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+2. film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+3. customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+Kolay Gelsin.*/
+
+--1
+select * from film where title like '%n' order by length desc limit 5;
+--2
+select * from film where title like '%n' order by length asc limit 10 offset 5;
+--3
+select * from customer where store_id=1 order by last_name desc limit 4;
